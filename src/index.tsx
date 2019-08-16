b3d8d290e33decdec41b84b21c09d19cf27b8fae
Frank Zickert <webmaster@clued-up.de>
2019-08-16 17:24:13 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import "@babel/polyfill";
+import {
+    callService,
+    Environment,
+    Middleware,
+    Route,
+    Service,
+    ServiceOrientedApp
+} from "infrastructure-components";
+
+const SERVICE_ID = "myservice";
+
+async function callMyService () {
+
+    await callService(
+        SERVICE_ID,
+        { some: "data" },
+        (data: any) => {
+            console.log("received data: ", data);
+
+        },
+        (error) => {
+            console.log("error: " , error)
+        }
+    );
+
+}
+
+export default (
+    <ServiceOrientedApp
+        stackName = "{§PROJECT_NAME§}"
+        buildPath = 'build'
+        region='eu-west-1'>
+
+        {§ENVIRONMENT_NAME§}
+
+        <Route
+            path='/'
+            name='My Service-Oriented React App'
+            render={()=><div>
+                <button onClick={callMyService}>Hello Infrastructure-Components!</button>
+            </div>}
+        />
+
+        <Service
+            id={ SERVICE_ID }
+            path="/myservice"
+            method="POST">
+
+            <Middleware
+                callback={ function (req, res, next) {
+                    const parsedBody = JSON.parse(req.body);
+
+                    console.log("this is the service: ", parsedBody);
+
+                    res.status(200).set({
+                        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+                    }).send("ok");
+
+            }}/>
+
+        </Service>
+    </ServiceOrientedApp>);
